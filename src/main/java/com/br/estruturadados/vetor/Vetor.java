6a5e2eb970530952ff4df8df7427c4e9ad1c7e22
GiselleBarbosa <g88barbosa@gmail.com>
2024-11-13 21:00:46 -0300

    remove comentarios

--- src/main/java/com/br/estruturadados/vetor/Vetor.java
+++ src/main/java/com/br/estruturadados/vetor/Vetor.java
@@ -38,24 +38,7 @@ public class Vetor {
 
     @Override
     public String toString() {
-
-       /* StringBuilder s = new StringBuilder();
-        s.append("[");
-
-        for (int i = 0; i < this.tamanho - 1; i++) {
-            s.append(this.elementos[i]);
-            s.append(", ");
-        }
-
-        if (this.tamanho > 0) {
-            s.append(this.elementos[this.tamanho - 1]);
-        }
-        s.append("]");
-
-        return s.toString();*/
-
-        // simplificando
-        return Arrays.toString(Arrays.copyOfRange(elementos, 0, tamanho));
-
+        return Arrays.toString(
+                Arrays.copyOfRange(elementos, 0, tamanho));
     }
 }
